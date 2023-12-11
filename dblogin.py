f3a4d3a3779cfc75bf454579316f64961af465b9
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2023-12-11 15:30:29 -0500

    verify() documented

--- dblogin.py
+++ dblogin.py
@@ -1,26 +1,30 @@
-#os is used to generate a cryptographically strong salt. Alternative: secrets
+# os is used to generate a cryptographically strong salt. Alternative: secrets
 import os
 
-#haslib is used for generating hashes
+# haslib is used for generating hashes
 import hashlib
 
 class Dblogin:
-    
-    #The user's password is hashed and is never stored as plain text.
+
+    # The user's password is hashed and is never stored as plain text.
     def __init__(self, username, password):
         self.username = username
         self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         self.salt = None
     
     '''
-    Description: This function is to be called once database connection has been made and there is a row with
-                matching username.
+    Description: This function is to be called once database connection
+                has been made and there is a record with matching username.
     @param dbPassword: The salted password stored retrieved from the database.
     @param salt: The salt retrieved from the database.
+    @returns boolean: True if the credentials match, else False.
     '''
     def verify(self, dbPassword, salt):
-        #Hashing the password user entered with the salt from the database
-        saltedPassword = hashlib.pbkdf2_hmac('sha256', self.password.encode('utf-8'), salt.encode('utf-8'), 100000).hex()[:32]
+        # Hashing the password user entered with the salt from the database
+        saltedPassword = hashlib.pbkdf2_hmac('sha256',
+                                             self.password.encode('utf-8'),
+                                             salt.encode('utf-8'),
+                                             100000).hex()[:32]
 
         if(dbPassword == saltedPassword): return True
         else: return False
